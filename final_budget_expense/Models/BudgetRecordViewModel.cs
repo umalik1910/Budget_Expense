54b51ef678482702c82d158e8439ca1d458b31dd
Utsav Malik <utsavmalik10@gmail.com>
2019-07-25 18:06:39 -0500

    develop push hopefully works

---
+++ final_budget_expense/Models/BudgetRecordViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace final_budget_expense.Models
+{
+    public class BudgetRecordViewModel
+    {
+        public BudgetRecord Records { get; set; }
+        public List<int> Years { get; set; }
+        public List<int> Months { get; set; }
+
+
+    }
+}
