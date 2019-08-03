70bc272f5cb8d4d4cca450fdeeafc07b0cd11d53
Utsav Malik <utsavmalik10@gmail.com>
2019-08-03 12:02:26 -0500

    only numbers allowed when submitting amount inside home page

--- final_budget_expense/Scripts/HomeController.js
+++ final_budget_expense/Scripts/HomeController.js
@@ -198,3 +198,19 @@ $(document).ready("#monthOptions").change(function () {
             }
         });
     }); 
+var inputBox = document.getElementById("amount_input");
+var invalidChars = [
+    "-",
+    "+",
+    "e",
+];
+
+inputBox.addEventListener("input", function () {
+    this.value = this.value.replace(/[e\+\-]/gi, "");
+});
+
+inputBox.addEventListener("keydown", function (e) {
+    if (invalidChars.includes(e.key)) {
+        e.preventDefault();
+    }
+});
