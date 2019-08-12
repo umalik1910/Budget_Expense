43b7f4343d5ea7c060e7aa90783445838c9c971c
Utsav Malik <utsavmalik10@gmail.com>
2019-08-12 10:55:31 -0500

    finished project, reworked css

--- final_budget_expense/Scripts/HomeController.js
+++ final_budget_expense/Scripts/HomeController.js
@@ -230,3 +230,7 @@ $("#reset_button").click(function () {
     $("#date_selection").val('');
     $(".description").val('');
 });
+var validate = function (e) {
+    var t = e.value;
+    e.value = (t.indexOf(".") >= 0) ? (t.substr(0, t.indexOf(".")) + t.substr(t.indexOf("."), 3)) : t;
+}
